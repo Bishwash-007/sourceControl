72be1c5f4cef8d8947f55e90c91bd2290e22b797
Bishwash Chaudhari <drewwbishwash@gmail.com>
2024-06-10 21:10:57 +0545

    Initial Commit

---
+++ sourceControl/sourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  sourceControlApp.swift
+//  sourceControl
+//
+//  Created by Bishwash Chaudhari on 10/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct sourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
