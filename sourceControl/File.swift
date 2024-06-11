38bf3c3c0f8a1536c491a69c61dfc71e69530087
Bishwash Chaudhari <117927932+Bishwashdrew@users.noreply.github.com>
2024-06-11 15:01:26 +0545

    Update File.swift

--- sourceControl/File.swift
+++ sourceControl/File.swift
@@ -1,17 +1,1 @@
-//
-//  File.swift
-//  sourceControl
-//
-//  Created by Bishwash Chaudhari on 10/06/2024.
-//
-
-import Foundation
-import SwiftUI
-
-var a = 5
-var b = 67
-var sum = a + b
-
-
-
-
+// just for sum
